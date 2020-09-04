ec3b2a9fa9e3d954de97f0a639f7ebd7402bdcd4
sabovyan <abovyan.sargis90@gmail.com>
2020-09-04 18:34:16 +0400

    created switchRoutes component

--- src/App.js
+++ src/App.js
@@ -1,58 +1,126 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  useRouteMatch,
+} from "react-router-dom";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/create">Create</Link>
-              </li>
-              <li>
-                <Link to="/article">Article</Link>
-              </li>
-              <li>
-                <Link to="/">Logout</Link>
-              </li>
-            </ul>
-          </nav>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loggedIn: true,
+    };
+  }
+  handleLogOut = (e) => {
+    this.setState((prevState) => ({
+      loggedIn: !prevState.loggedIn,
+    }));
+  };
 
-          {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-          <Switch>
-            <Route path="/create">
-              <About />
-            </Route>
-            <Route path="/article">
-              <Users />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    </div>
-  );
+  handleNavLinkActivation = (e) => {
+    if (!this.state.loggedIn) {
+      e.preventDefault();
+    }
+  };
+
+  render() {
+    const { loggedIn } = this.state;
+    return (
+      <div className="App">
+        <Router>
+          <header className="nav__header">
+            <div className="nav__logo">
+              <MenuLink activeOnlyWhenExact={true} to="/" label="Blog" />
+            </div>
+            <nav className="nav__main">
+              <ul className="nav__list">
+                <li className="nav__list-item">
+                  <MenuLink
+                    onClick={this.handleNavLinkActivation}
+                    to="/create"
+                    label="Create"
+                  />
+                </li>
+                <li className="nav__list-item">
+                  <MenuLink
+                    onClick={this.handleNavLinkActivation}
+                    to="/read"
+                    label="Read"
+                  />
+                </li>
+                <li hidden={!loggedIn} className="nav__list-item">
+                  <Link
+                    onClick={this.handleLogOut}
+                    className="nav__link"
+                    to="/"
+                  >
+                    Log out
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </header>
+
+          <SwitchRoutes />
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
 
 function Home() {
-  return <h2>Home</h2>;
+  return (
+    <div className="Home">
+      <h2>Home</h2>
+      <button>log In</button>
+    </div>
+  );
 }
 
-function About() {
-  return <h2>About</h2>;
+function Create() {
+  return <h2>Create</h2>;
 }
 
-function Users() {
-  return <h2>Users</h2>;
+function Read() {
+  return <h2>Read</h2>;
+}
+
+class SwitchRoutes extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/create">
+          <Create />
+        </Route>
+        <Route path="/read">
+          <Read />
+        </Route>
+      </Switch>
+    );
+  }
+}
+
+function MenuLink({ label, to, activeOnlyWhenExact, onClick }) {
+  let match = useRouteMatch({
+    path: to,
+    exact: activeOnlyWhenExact,
+  });
+  return (
+    <Link
+      onClick={onClick}
+      className={match ? " nav__link active" : "nav__link"}
+      to={to}
+    >
+      {label}
+    </Link>
+  );
 }
