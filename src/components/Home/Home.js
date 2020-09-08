d8565745d9d0c4ce7ec5529edf5ffe59e5b930b3
sabovyan <abovyan.sargis90@gmail.com>
2020-09-08 22:04:58 +0400

    removed login form from home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,20 +1,14 @@
 import React, { Component } from 'react';
-
-import TextField from '@material-ui/core/TextField';
-import FormControl from '@material-ui/core/FormControl';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import IconButton from '@material-ui/core/IconButton';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import Visibility from '@material-ui/icons/Visibility';
-import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import { Link } from 'react-router-dom';
 import Banner from '../Banner/Banner';
+// import LoginForm from '../LoginForm/LoginForm';
+
 export default class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
       LoginDisplayed: false,
-      PasswordValue: {
+      password: {
         showPassword: false,
         value: '',
       },
@@ -27,57 +21,27 @@ export default class Home extends Component {
     }));
   };
 
-  handleChange = ({ target: { value } }) => {
+  handlePasswordChange = ({ target: { value } }) => {
     this.setState({
-      PasswordValue: {
+      password: {
         value,
       },
     });
   };
 
-  handleClickShowPassword = () => {
-    this.setState((prevState) => ({
-      PasswordValue: {
-        showPassword: !prevState.PasswordValue.showPassword,
-      },
-    }));
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+    if (this.state.formValidated) {
+    }
   };
 
+  handleClickShowPassword = () => {};
+
   render() {
-    const { LoginDisplayed, PasswordValue } = this.state;
+    const { LoginDisplayed, password } = this.state;
     return (
       <div className="Home">
         <Banner onClick={this.handleBannerButtonClicked} />
-        <div hidden={!LoginDisplayed} className="form__container">
-          <form autoComplete="off">
-            <TextField id="standard-basic" label="log in" />
-            <FormControl>
-              <InputLabel htmlFor="standard-adornment-password">
-                Password
-              </InputLabel>
-              <Input
-                id="standard-adornment-password"
-                type={PasswordValue.showPassword ? 'text' : 'password'}
-                value={PasswordValue.value}
-                onChange={this.handleChange}
-                endAdornment={
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={this.handleClickShowPassword}
-                    >
-                      {PasswordValue.showPassword ? (
-                        <Visibility />
-                      ) : (
-                        <VisibilityOff />
-                      )}
-                    </IconButton>
-                  </InputAdornment>
-                }
-              />
-            </FormControl>
-          </form>
-        </div>
       </div>
     );
   }
