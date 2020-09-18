19aa051ccbbaa75e1d8ceaf69d91df992bf9668c
sabovyan <abovyan.sargis90@gmail.com>
2020-09-19 02:26:16 +0400

    changed Create.js

--- src/pages/Create/Create.js
+++ src/pages/Create/Create.js
@@ -1,94 +1,96 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 import TextField from '@material-ui/core/TextField';
-import TextareaAutosize from '@material-ui/core/TextareaAutosize';
 
 import FormTheme from '../../components/FormTheme/FormTheme';
 
 import './Create.css';
 
-class Create extends Component {
-  render() {
-    return (
-      <div className="create">
-        <h1 className="create__title">Add your Post</h1>
-        <form className="create__form">
-          <FormTheme>
-            <TextField
-              variant="outlined"
-              label="title"
-              className="create__field create__title"
-              style={{
-                marginBottom: 10,
-                width: '80%',
-              }}
-            />
-            <TextField
-              onChange={this.handleKeywordsInput}
-              variant="outlined"
-              label="keywords"
-              InputLabelProps={{
-                shrink: true,
-              }}
-              helperText="Incorrect entry."
-              className="create__keywords create__field"
-              style={{
-                marginBottom: 10,
-                width: '80%',
-              }}
-            />
-            <TextareaAutosize
-              rowsMin={20}
-              rowsMax={30}
-              aria-label="maximum height"
-              placeholder="Use markdown syntax for your text"
-              defaultValue=""
-              style={{
-                marginBottom: 10,
-                fontSize: 20,
-                fontFamily: 'Roboto',
-                padding: 10,
-                width: '100%',
-              }}
-            />
+function Create() {
+  const [title, setTitle] = useState('');
+  const handleTitleInput = ({ target: { value } }) => setTitle(value);
 
-            <ButtonGroup
-              aria-label="button group"
+  const [keywords, setKeywords] = useState('');
+  const handleKeywordsInput = ({ target: { value } }) => setKeywords(value);
+
+  const [post, setPost] = useState('');
+  const handleTextAreaInput = ({ target: { value } }) => setPost(value);
+
+  const handleKeywordsEnter = () => {};
+
+  return (
+    <div className="create">
+      <h1 className="create__title">Add your Post</h1>
+      <div className="create__form">
+        <FormTheme>
+          <TextField
+            value={title}
+            onChange={handleTitleInput}
+            variant="outlined"
+            label="title"
+            className="create__field create__title"
+            style={{
+              marginBottom: 10,
+              width: '80%',
+            }}
+          />
+          <TextField
+            value={keywords}
+            onChange={handleKeywordsInput}
+            onKeyDown={handleKeywordsEnter}
+            variant="outlined"
+            label="keywords"
+            helperText="#Language #Topic #feature"
+            className="create__keywords create__field"
+            style={{
+              marginBottom: 10,
+              width: '80%',
+            }}
+          />
+          <textarea
+            className="create__textarea"
+            value={post}
+            onChange={handleTextAreaInput}
+            rows={20}
+            placeholder="Use markdown syntax for your post"
+          />
+
+          <ButtonGroup
+            aria-label="button group"
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              width: '100%',
+              gap: 10,
+            }}
+          >
+            <Button
+              color="primary"
               style={{
-                display: 'flex',
-                justifyContent: 'space-between',
-                width: '100%',
-                gap: 10,
+                border: '1px solid',
+                flexGrow: 2,
+                fontSize: 20,
+                borderRadius: 5,
               }}
             >
-              <Button
-                color="primary"
-                style={{
-                  border: '1px solid',
-                  flexGrow: 2,
-                  fontSize: 20,
-                  borderRadius: 5,
-                }}
-              >
-                Submit
-              </Button>
-              <Button
-                color="default"
-                style={{
-                  flexGrow: 0.9,
-                  fontSize: 20,
-                  borderRadius: 5,
-                }}
-              >
-                Preview
-              </Button>
-            </ButtonGroup>
-          </FormTheme>
-        </form>
+              Submit
+            </Button>
+            <Button
+              color="default"
+              style={{
+                flexGrow: 0.9,
+                fontSize: 20,
+                borderRadius: 5,
+              }}
+            >
+              Preview
+            </Button>
+          </ButtonGroup>
+        </FormTheme>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Create;
