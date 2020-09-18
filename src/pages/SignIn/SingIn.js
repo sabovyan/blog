0e57a0e3b03dfc905609d06748b60dae2b23d6bd
sabovyan <abovyan.sargis90@gmail.com>
2020-09-18 19:59:17 +0400

    changed login structure

---
+++ src/pages/SignIn/SingIn.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import login from '../../components/Login/Login';
+
+function SignIn() {
+  return (
+    <>
+      <login />
+    </>
+  );
+}
+
+export default SignIn;
