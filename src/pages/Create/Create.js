24fc4cde41d02eb0f2c7d5f68be8fcb10e7ea891
sabovyan <abovyan.sargis90@gmail.com>
2020-09-19 18:25:18 +0400

    added tags functionality

--- src/pages/Create/Create.js
+++ src/pages/Create/Create.js
@@ -1,86 +1,163 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 import TextField from '@material-ui/core/TextField';
-
+import uniqueId from 'lodash/uniqueId';
 import FormTheme from '../../components/FormTheme/FormTheme';
 
+import Tags from '../../components/Tags/Tags';
 import './Create.css';
 
 function Create() {
   const [title, setTitle] = useState('');
-  const handleTitleInput = ({ target: { value } }) => setTitle(value);
-
   const [keywords, setKeywords] = useState('');
-  const handleKeywordsInput = ({ target: { value } }) => setKeywords(value);
-
   const [post, setPost] = useState('');
+  const [tags, setTags] = useState([]);
+  const [keywordError, setKeywordError] = useState(null);
+
+  const handleTitleInput = useCallback(
+    ({ target: { value } }) => setTitle(value),
+    []
+  );
+
+  const handleKeywordsInput = useCallback(
+    ({ target: { value } }) => setKeywords(value),
+    []
+  );
+
+  /* TODO check if this function needs useCallback */
   const handleTextAreaInput = ({ target: { value } }) => setPost(value);
 
-  const [tags, setTag] = useState([]);
   const handleKeywordsEnter = (event) => {
+    setKeywordError(null);
+
     const { value } = event.target;
-    console.log(event.key);
-    if (event.key === 'Enter' || event.key === ' ') {
+    const { key } = event;
+
+    if (key === 'Enter' || key === ' ') {
       if (value.trim() !== '') {
         const newTag = {
-          id: 0,
-          value,
+          id: uniqueId('tag_'),
+          value: value.trim(),
           isEdit: false,
         };
-        setTag((t) => [...t, value.trim()]);
-        setKeywords('');
+        for (const tag of tags) {
+          if (tag.value === value) {
+            setKeywordError('there two tags that are the same');
+            return;
+          }
+        }
+
+        if (tags.length <= 2) {
+          setTags((t) => [...t, newTag]);
+          setKeywords('');
+        } else {
+          setKeywordError('Only three tags are allowed');
+          setKeywords('');
+        }
       }
     }
   };
 
-  const handleKeywordDelete = () => {};
+  const handleTagDelete = (id) => () => {
+    setTags((tags) => tags.filter((tag) => tag.id !== id));
+  };
+
+  const handleTagEdit = (id) => () => {
+    setTags((tags) =>
+      tags.map((t) =>
+        t.id === id ? { ...t, isEdit: !t.isEdit, draftValue: t.value } : t
+      )
+    );
+  };
+
+  const handleTagEditInput = (id) => (event) => {
+    const { value } = event.target;
+    setTags(
+      tags.map((t) => {
+        return t.id === id ? { ...t, draftValue: value } : t;
+      })
+    );
+  };
+
+  const handleTagSubmitInput = (id) => (event) => {
+    const { value } = event.target;
+    const { key } = event;
+    console.log(key);
+
+    if (key === 'Enter' || key === ' ') {
+      if (value.trim() === '') {
+        setTags(
+          tags.map((t) => {
+            return t.id === id ? { ...t, isEdit: !t.isEdit } : t;
+          })
+        );
+      }
+      if (value.trim() !== '') {
+        setTags(
+          tags.map((t) => {
+            return t.id === id
+              ? { ...t, value: t.draftValue, isEdit: !t.isEdit }
+              : t;
+          })
+        );
+      }
+    }
+  };
+
+  const handleTagInputBlur = (id) => (event) => {
+    setTags((tags) =>
+      tags.map((t) => (t.id === id ? { ...t, isEdit: !t.isEdit } : t))
+    );
+  };
 
   return (
     <div className="create">
       <h1 className="create__title">Add your Post</h1>
       <div className="create__form">
         <FormTheme>
-          <TextField
-            value={title}
-            onChange={handleTitleInput}
-            variant="outlined"
-            label="Title"
-            className="create__field create__field-title"
-            style={{
-              marginBottom: 10,
-              width: '80%',
-            }}
-          />
-          <div className="keywords__container">
+          <div className="input__container">
+            <TextField
+              value={title}
+              autoFocus
+              onChange={handleTitleInput}
+              variant="outlined"
+              label="Title"
+              className="create__field create__field-title"
+              style={{
+                margin: 10,
+                width: '50%',
+              }}
+            />
             <TextField
               value={keywords}
               onChange={handleKeywordsInput}
               onKeyDown={handleKeywordsEnter}
               variant="outlined"
               label="Keywords"
-              helperText="#Language #Topic #feature"
+              color={keywordError ? 'secondary' : 'primary'}
+              helperText={
+                keywordError === null
+                  ? '#Language #Topic #feature'
+                  : keywordError
+              }
               className="create__keywords create__field"
               style={{
-                marginBottom: 10,
+                margin: 10,
                 width: '50%',
               }}
             />
-
-            <ul className="added-keywords">
-              {tags.map((tag) => (
-                <li className="added-keyword" key={tag}>
-                  <span>{`#${tag}`}</span>
-                  <span
-                    onClick={handleKeywordDelete}
-                    className="delete-keyword"
-                  >
-                    &#x292B;
-                  </span>
-                </li>
-              ))}
-            </ul>
           </div>
+
+          <Tags
+            tags={tags}
+            handleTagEditInput={handleTagEditInput}
+            handleTagSubmitInput={handleTagSubmitInput}
+            handleTagInputBlur={handleTagInputBlur}
+            handleTagEdit={handleTagEdit}
+            handleTagDelete={handleTagDelete}
+          />
+
           <textarea
             className="create__textarea"
             value={post}
@@ -127,10 +204,3 @@ function Create() {
 }
 
 export default Create;
-
-function getLastWord(value) {
-  const temp = value.trim().split(' ');
-  const result = temp.slice(-1);
-
-  return result;
-}
